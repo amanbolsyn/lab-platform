c912c864c1be8621de82e4715721e8325903c5f6
Aman Bolsyn <nurgozhiyevaman@gmail.com>
2026-03-21 19:34:24 +0500

    feat: added email verification

--- app/Http/Controllers/Api/v1/RegisterController.php
+++ app/Http/Controllers/Api/v1/RegisterController.php
@@ -20,18 +20,51 @@ class RegisterController extends Controller
     public function store(RegisterUserRequest $request)
     {
         $userAttributes = collect($request->input('data.attributes'));
-        $programId = ['program_id' => $request->input('relationships.program.id')]; 
+        $programId = ['program_id' => $request->input('relationships.program.id')];
 
         $user = User::create(array_merge($userAttributes->toArray(), $programId));
 
         $role = Role::where('role', 'user')->first();
         $user->roles()->attach($role->id);
 
-        // event(new Registered($user));
+        event(new Registered($user));
 
         return $this->success("Registred", [
             'token' => $user->createToken('token' . $user->email, ['*'],  now()->plus(minutes: 40))->plainTextToken,
             'includes' =>   new UserResource($user)
         ]);
+    }
+
+    public function verifyEmail(Request $request, $id, $hash)
+    {
+        $user = User::findOrFail($id);
+
+        // Verify if the hash is correct
+        if (!hash_equals((string) $hash, sha1($user->getEmailForVerification()))) {
+            return response()->json(['message' => 'Invalid verification link.'], 403);
+        }
+
+        // Mark email as verified
+        if (!$user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+        }
+
+        return response()->json(['message' => 'Email verified successfully!']);
+    }
+
+    public function sendVertifiction(Request $request) {
+
+         $user = User::where('email', $request->email)->first();
+    
+        // if (!$user) {
+        //     return response()->json(['message' => 'User not found.'], 404);
+        // }
+    
+        if ($user->hasVerifiedEmail()) {
+            return response()->json(['message' => 'Email already verified.'], 400);
+        }
+    
+        $user->sendEmailVerificationNotification();
+        return response()->json(['message' => 'Verification link resent!']);
     }
 }
